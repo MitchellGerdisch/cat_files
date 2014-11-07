6022b3df06e95bbfa2fc6d3ef1154da9189d6328
MitchellGerdisch <mitchell.gerdisch@rightscale.com>
2014-11-07 07:53:33 -0600

    tweaks

--- HostedChefClient/HostedChefClient.rb
+++ HostedChefClient/HostedChefClient.rb
@@ -27,21 +27,31 @@
 # Deploys a single Chef Client server that connects to an OpsCode hosted chef server.
 #
 # PREREQUISITES:
+# Cloud Management:
 #   Imported Server Templates:
 #     Chef Client Beta (v13.5.1) [rev 32] 
+#   Credential: GERDISCH_OPSCODE_VALIDATION_KEY 
+#     Or the key for the opscode user specified below.
 #
-#   OpsCode Hosted Chef Setup 
-#     Account on OpsCode hosted chef.
-#     Organization defined
-#     Role and related cookbooks defined (optional but recommended)
-# DEMO NOTES:
-#   TBD
+# OpsCode Hosted Chef
+#   Account on OpsCode hosted chef.
+#   Organization defined: rs_demo
+#
+# Demo Notes:
+#   - Pick a role when deploying.
+#   - Click link to server to see the deployed role.
+#   - Apply other role after deployment.
+#   - Refresh link to see new role.
 
-name "Hosted Chef Client"
+name "Chef Client with Hosted Chef"
 rs_ca_ver 20131202
-short_description "![Chef](https://www.getchef.com/images/logo.svg)
-Builds a Chef Client server that connects to a hosted chef server.
-NO ROLE SUPPORT AT THIS TIME."
+short_description "![Chef](https://www.getchef.com/images/logo.svg)\n
+Builds a Chef Client server that connects to a hosted chef server."
+long_description "Deploy using one of the two preconfigured roles: turner or hooch.\n
+After deployment click the link to see the web server display.\n
+Use More Actions to change to the other role.\n
+After updated with the new role, refresh the web page.\n
+\nYou can also see the client registration with the Chef server by accessing the opscode account. Contact Mitch Gerdisch to be invited to the opscode account."
 
 ##############
 # PARAMETERS #
@@ -67,9 +77,9 @@ end
 
 parameter "param_role" do 
   category "Chef"
-  label "Chef Role(s)" 
+  label "Chef Role" 
   type "string" 
-  description "Chef role(s) to apply to the server." 
+  description "Chef role to apply to the server." 
 #  allowed_pattern "[A-Za-z0-9][A-Za-z0-9-_.]*"
   allowed_values "turner", "hooch"
   default "hooch"
@@ -80,6 +90,15 @@ end
 ##############
 # MAPPINGS   #
 ##############
+
+mapping "map_hosted_chef_account" do {
+  "opscode_hosted_chef" => {
+    "chef_org" => "rs_demo",
+    "chef_username" => "gerdisch",
+    "chef_user_validation" => "GERDISCH_OPSCODE_VALIDATION_KEY",
+  }
+}
+end
 
 mapping "map_instance_type" do {
   "AWS" => {
@@ -148,18 +167,11 @@ mapping "map_account" do {
   "CSE Sandbox" => {
     "security_group" => "default",
     "ssh_key" => "default",
-    "chef_org" => "rs_demo",
-    "chef_username" => "gerdisch",
-    "chef_user_validation" => "GERDISCH_OPSCODE_VALIDATION_KEY",
   },
-#  "Hybrid Cloud" => {
-#    "security_group" => "IIS_3tier_default_SecGrp",
-#    "ssh_key" => "default",
-#    "s3_bucket" => "iis-3tier",
-#    "restore_db_script_href" => "493424003",
-#    "create_db_login_script_href" => "493420003",
-#    "restart_iis_script_href" => "527791003",
-#  },
+  "Hybrid Cloud" => {
+    "security_group" => "IIS_3tier_default_SecGrp",
+    "ssh_key" => "default",
+  },
 }
 end
 
@@ -183,7 +195,14 @@ output "server_url" do
   label "Server URL" 
   category "Connect"
   default_value join(["http://", @client_server.public_ip_address])
-  description "Assuming the server supports a webservice on port 80, this link can be used to access it."
+  description "Assuming the role supports a webservice on port 80, this link can be used to access it."
+end
+
+output "hosted_chef_url" do
+  label "Show Registered Chef Client Nodes"
+  category "Connect"
+  default_value "https://manage.opscode.com"
+  description "Link to the hosted chef site."
 end
 
 ##############
@@ -199,10 +218,10 @@ resource "client_server", type: "server" do
   security_groups switch($inAWS, map($map_account, map($map_current_account, "current_account_name", "current_account"), "security_group"), null)
   inputs do {
     "chef/client/roles" => join(["text:", $param_role]),
-    "chef/client/company" => join(["text:", map($map_account, map($map_current_account, "current_account_name", "current_account"), "chef_org")]),
-    "chef/client/server_url" => join(["text:https://api.opscode.com/organizations/", map($map_account, map($map_current_account, "current_account_name", "current_account"), "chef_org")]),
-    "chef/client/validation_name" => join(["text:", map($map_account, map($map_current_account, "current_account_name", "current_account"), "chef_username")]),
-    "chef/client/validator_pem" => join(["cred:", map($map_account, map($map_current_account, "current_account_name", "current_account"), "chef_user_validation")]),
+    "chef/client/company" => join(["text:", map($map_hosted_chef_account, "opscode_hosted_chef", "chef_org")]),
+    "chef/client/server_url" => join(["text:https://api.opscode.com/organizations/", map($map_hosted_chef_account, "opscode_hosted_chef", "chef_org")]),
+    "chef/client/validation_name" => join(["text:", map($map_hosted_chef_account, "opscode_hosted_chef", "chef_username")]),
+    "chef/client/validator_pem" => join(["cred:", map($map_hosted_chef_account, "opscode_hosted_chef", "chef_user_validation")]),
     "chef/client/log_level" => "text:debug",  
   } end
 end
@@ -212,7 +231,15 @@ end
 ## Operations #
 ###############
 
-# executes automatically
+operation "enable" do
+  description "Enable the application."
+  definition "enable_application"
+  output_mappings do {
+    $hosted_chef_url => join(["https://manage.opscode.com/organizations/", $chef_org, "/nodes"])
+  } end
+end
+
+# Allows user to apply different role after app is running
 operation "Apply Role" do
   description "Apply role to server."
   definition "apply_role"
@@ -223,9 +250,22 @@ end
 # Definitions#
 ##############
 
+# 
+# Enable Application
+# Generates a better chef_url output value to take user directly to the Nodes listing.
+#
+define enable_application(@client_server, $map_hosted_chef_account) return $chef_org do
+  task_label("Enabling application")
+  
+  # Return the chef org for the updated mapping.
+  # Was not able to see a way to use the mapping directly in the output or output_mapping.
+  $chef_org = map($map_hosted_chef_account, "opscode_hosted_chef", "chef_org")
+
+end
+
+
 #
 # Apply Role
-# NOT READY YET
 #
 define apply_role(@client_server, $param_role) do
   task_label("Apply role")
