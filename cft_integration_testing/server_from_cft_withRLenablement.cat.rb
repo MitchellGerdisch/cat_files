91b3e7abbada35116ebc1743215ae7db71c507f5
MitchellGerdisch <mitchell.gerdisch@rightscale.com>
2018-01-04 16:10:48 -0600

    new cft-oriented cat

--- cft_integration_testing/server_from_cft_withRLenablement.cat.rb
+++ cft_integration_testing/server_from_cft_withRLenablement.cat.rb
@@ -1,10 +1,31 @@
+# Simple example of launching an instance via CFT and then RightLink enabling the launched instance.
+# 
+# REQUIRES a credential named "PFT_RS_REFRESH_TOKEN" in the account that contains a RightScale refresh token with
+# permissions to RL enable a server.
+
 name "CFT launched server with RL"
 rs_ca_ver 20161221
 short_description  "CFT launched server with RL enablement applied afterwards"
 
 import "plugins/rs_aws_cft"
 import "pft/err_utilities", as: "debug"
-import "aws_rightlink_enablement", as: "rl_enable"
+import "rl_enable/aws", as: "rl_enable"
+
+output "output_server_public_ip" do
+  label "Server Public IP Address"
+end
+
+output "output_server_private_ip" do
+  label "Server Private IP Address"
+end
+
+mapping "map_server_type" do {
+  "Linux" => {
+    "cft" => "https://s3.amazonaws.com/rs-hybrid-demo-cfts/EC2InstanceWithSecurityGroupNonVPC.template",
+#    "cft" => "https://s3-us-west-2.amazonaws.com/cloudformation-templates-us-west-2/EC2InstanceWithSecurityGroupSample.template",
+    "st" => "RightLink 10.6.0 Linux Base"
+  }
+} end
 
 ### SSH key declarations ###
 resource "ssh_key", type: "ssh_key" do
@@ -14,7 +35,7 @@ end
 
 resource "stack", type: "rs_aws_cft.stack" do
   stack_name join(["cft-", last(split(@@deployment.href, "/"))])
-  template_url "https://s3-us-west-2.amazonaws.com/cloudformation-templates-us-west-2/EC2InstanceWithSecurityGroupSample.template"
+  template_url map($map_server_type, "Linux", "cft")
   description "CFT Test"
   parameter_1_name "KeyName"
   parameter_1_value @ssh_key.name
@@ -23,9 +44,25 @@ end
 
 operation "enable" do
   definition "post_launch"
+  output_mappings do {
+    $output_server_public_ip => $server_public_ip,
+    $output_server_private_ip => $server_private_ip
+  } end
 end
 
-define post_launch(@stack) do
+operation "terminate" do
+  definition "terminator"
+end
+
+operation "stop" do
+  definition "stopper"
+end
+
+operation "start" do
+  definition "starter"
+end
+
+define post_launch(@stack, $map_server_type) return $server_public_ip, $server_private_ip do
   call debug.log("output values", to_s(@stack.OutputValue))
   
   # Find the instance attributes
@@ -57,7 +94,44 @@ define post_launch(@stack) do
   call debug.log("@instance", to_s(to_object(@instance)))
 
   # Now go off and turn it into a RightScale managed server
-  call rl_enable.rightlink_enable(@instance, "RightLink 10.6.0 Linux Base") 
+  call rl_enable.rightlink_enable(@instance, map($map_server_type, "Linux", "st"), "PFT_RS_REFRESH_TOKEN") 
+    
+  @server = rs_cm.servers.get(filter: ["deployment_href=="+@@deployment.href])
+  $server_public_ip = @server.current_instance().public_ip_addresses[0]
+  $server_private_ip = @server.current_instance().private_ip_addresses[0]
+
+  # Stop the CAT after 5 minutes.
+  $time = now() + 300
+  rs_ss.scheduled_actions.create(
+    execution_id: @@execution.id,
+    action: "stop",
+    first_occurrence: $time
+  )
   
+end
+
+define terminator() do
+  # Concurrently terminate all the servers
+  concurrent foreach @server in @@deployment.servers() do
+    delete(@server)
+  end
+end
+
+define stopper() do
+  @instances = @@deployment.servers().current_instance()
+  call rl_enable.stop_instances(@instances) retrieve @stopped_instances
+end
+
+define starter() do
+  @instances = @@deployment.servers().current_instance()
+  call rl_enable.start_instances(@instances) 
+  
+  # Stop the CAT after 5 minutes.
+  $time = now() + 300
+  rs_ss.scheduled_actions.create(
+    execution_id: @@execution.id,
+    action: "stop",
+    first_occurrence: $time
+  )
 end
 
